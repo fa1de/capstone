c7c5a7ed4bc53efb973fe699beb1159f4989ca0a
acon <pvc0070@daum.net>
2024-03-26 20:24:42 +0900

    deleted:    README.md 	modified:   pcap.py

--- pcap.py
+++ pcap.py
@@ -1,6 +1,5 @@
 from scapy.all import sniff, wrpcap
 import pyshark
-#from scapy.layers.inet import 프로토콜명  // 특정 프로토콜만 캡쳐하기 위해 임포트
 
 def packet_sniffer(packet):
     print(packet.summary())
@@ -8,12 +7,10 @@ def packet_sniffer(packet):
 
 def pyshark_sniffer():
     capture = pyshark.LiveCapture(interface='ethernet')
-    for packet in capture.sniff_continuously(packet_count=10):
+    for packet in capture.sniff_continuously():
         print(packet)
-        #file_path = "저장 파일경로 작성"
+        
         with open('captured_packets.txt', 'a') as f:
             f.write(str(packet) + '\n')
 
-sniff(prn=packet_sniffer, count=10)
-#sniff(prn=packet_sniffer, filter="tcp", count=10) // 특정 프로토콜 필터링을 걸기 위함
-#pyshark_sniffer()
+sniff(prn=packet_sniffer)  # count 매개변수를 지정하지 않아서 패킷 감시를 무제한으로 설정합니다.
