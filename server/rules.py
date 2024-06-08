fe1b437dfc331868af39a99d12841a7170df966a
khw0828 <97077535+gusdn0828@users.noreply.github.com>
2024-06-08 18:20:49 +0900

    chore: add log into gitignore

--- server/rules.py
+++ server/rules.py
@@ -1,4 +1,6 @@
 import re
-from edge import extract_protocol_info
 
-p = re.match("TCP", extract_protocol_info)
+tcp_packet = re.compile(
+    r"sport\s+=\s+(?P<source_port>\w+).*dport\s+=\s+(?P<dest_port>\w+).*seq\s+=\s+(?P<seq_num>\d+).*ack\s+=\s+(?P<ack_num>\d+)",
+    re.DOTALL,
+)
